f6f42a21a1c5c5624ec6aada69decaba2376890a
CarlosARetuertoD <cretuertodelgado@gmail.com>
2025-04-26 23:53:11 -0500

    Commit

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, re_path
 from django.views.generic import TemplateView
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))] 
+# Solo añadir MEDIA_URL en desarrollo si quieres
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# **MUY IMPORTANTE: Solo capturar re_path para rutas que NO sean /static o /media/**
+urlpatterns += [
+    re_path(r'^(?!static/|media/).*$', TemplateView.as_view(template_name="index.html")),
+]
