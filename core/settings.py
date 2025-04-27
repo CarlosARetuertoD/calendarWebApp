f7db29c2051813f67e77af22b9f62aa56933efff
CarlosARetuertoD <cretuertodelgado@gmail.com>
2025-04-26 21:27:37 -0500

    Commit

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['calendarapp-wauo.onrender.com','localhost', '127.0.0.1',]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
