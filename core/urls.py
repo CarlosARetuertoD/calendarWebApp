7993ec276653751dccd65780975b9327e7991756
CarlosARetuertoD <cretuertodelgado@gmail.com>
2025-05-06 21:38:52 -0500

    Settings

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name="index.html")),]
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^(?!admin/|static/|media/).*$', TemplateView.as_view(template_name="index.html")),
+]
