1b860b467b31faaa1d23f7f08a2e94613b01fe32
CarlosARetuertoD <cretuertodelgado@gmail.com>
2025-05-24 18:52:46 -0500

    Render deploy v1

--- core/settings.py
+++ core/settings.py
@@ -86,18 +86,32 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 #DATABASES = {
- #   'default': dj_database_url.config(
-  #     default='sqlite:///db.sqlite3',
-   # )
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 #}
-AUTH_PASSWORD_VALIDATORS = []
+
+DATABASES = {
+    'default': dj_database_url.config(
+       default='sqlite:///db.sqlite3',
+    )
+}
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 LANGUAGE_CODE = 'es-es'
 TIME_ZONE = 'UTC'
